8a3700806d63e4fc157caf67d57c13ae9c2dbc7f
2382546457 <2382546457@qq.com>
2023-01-11 14:33:16 +0800

    变量的声明

---
+++ src/gocode/Test-project01/unit4/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+var a1 = 10
+
+func main() {
+	fmt.Println("你好")
+	var age int = 18
+	fmt.Printf("age为: %d\n", age)
+	var money  = 12.2
+	fmt.Println(money) 
+
+	sex := "男"
+	fmt.Println("性别为", sex)
+
+	var a1, a2, a3 = 10, 20, 30
+	fmt.Println("a1:", a1)
+	fmt.Println("a2:", a2)
+	fmt.Println("a3:", a3)
+
+	var c1, c2, c3 int
+	fmt.Println(c1, c2, c3)
+
+	
+
+}
