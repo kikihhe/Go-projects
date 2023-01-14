a1921ad12e1e003e2c9b3ba38336b419f1c4a0de
2382546457 <2382546457@qq.com>
2023-01-14 12:25:18 +0800

    内置函数: new(Type) *Type

---
+++ src/gocode/Test-project02/内置函数/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt、time、strconv这种函数是需要导包的函数
+	// 像 print、println、len，这样不需要导包的函数就是Go内置的函数
+
+	// new(Type) *Type 分配空间  主要分配值类型，如: int float bool string 数组 结构体
+	// 形参为需要分配的类型
+	// 返回值为该类型的指针，指向地址中存储一个该类型的默认值  new(int) 分配一个int大小的内存，返回的值是一个 *int指针。
+	i := new(int)
+	fmt.Printf("i的类型为: %T\n", i)
+	fmt.Println("i的值为", *i)
+
+	// make(t Type, size ...IntegerType) 主要分配引用类型
+	// 未完待续...
+
+}
